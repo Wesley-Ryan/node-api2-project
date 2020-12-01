1f1ae45c9b0f387cc842641dbac20fce59542ea6
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-01 13:07:21 -0800

    get post by id

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -11,10 +11,32 @@ router.get("/", async (req, res) => {
     const posts = await Helper.find();
     res.status(200).json(posts);
   } catch (error) {
-    res.json(error.message);
+    res.status(500).json({
+      error: "The posts information could not be retrieved.",
+      message: error.message,
+    });
   }
 });
+
 //get /api/posts/:id
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const selectedPost = await Helper.findById(id);
+    if (selectedPost.length <= 0) {
+      res
+        .status(404)
+        .json({ message: "The post with the specified ID does not exist." });
+    } else {
+      res.status(200).json(selectedPost);
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: "The posts information could not be retrieved.",
+      message: error.message,
+    });
+  }
+});
 
 //delete /api/posts/:id
 
