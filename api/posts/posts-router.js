c3b8e34a3bf09be13399dab1f2d03128eead10cf
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-12-01 17:37:26 -0800

    Able to delete

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -17,12 +17,10 @@ router.post("/", async (req, res) => {
       await Helper.insert(post);
       res.status(201).json(post);
     } catch (error) {
-      res
-        .status(500)
-        .json({
-          message: "There was an error while saving the post to the database",
-          error: error.message,
-        });
+      res.status(500).json({
+        message: "There was an error while saving the post to the database",
+        error: error.message,
+      });
     }
   }
 });
@@ -61,7 +59,29 @@ router.get("/:id", async (req, res) => {
 });
 
 //delete /api/posts/:id
-
+router.delete("/:id", async (req, res) => {
+  const { id } = req.params;
+  const postToRemove = await Helper.findById(id);
+  if (postToRemove.length <= 0) {
+    res
+      .status(404)
+      .json({ message: "The post with the specified ID does not exist." });
+  } else {
+    try {
+      Helper.remove(postToRemove);
+      res.status(201).json({ message: "the post was removed." });
+    } catch (error) {
+      res.status(500).json({
+        error: "The post could not be removed",
+        message: error.message,
+      });
+    }
+  }
+});
 //put /api/posts/:id
+router.put("/:id", (req, res) => {
+  try {
+  } catch (error) {}
+});
 
 module.exports = router;
